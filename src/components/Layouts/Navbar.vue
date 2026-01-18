8b4dc1f76cc5c6b00f8675bab6ddc03a237a6a88
nrlizza <nurul.izza@students.paramadina.ac.id>
2026-01-18 20:20:27 +0700

    [FIX] Navbar

--- src/components/Layouts/Navbar.vue
+++ src/components/Layouts/Navbar.vue
@@ -101,24 +101,6 @@ const navigateTo = (path) => {
                 Poin Lomba
               </router-link>
             </li>
-            <li>
-              <router-link to="/#"
-                class="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300">
-                <!-- SVG icon would go here -->
-                <component :is="SettingsIcon"
-                  class="text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-300" />
-                Settings
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/#"
-                class="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300">
-                <!-- SVG icon would go here -->
-                <component :is="InfoCircleIcon"
-                  class="text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-300" />
-                Support
-              </router-link>
-            </li>
           </ul>
 
           <router-link to="/login" @click="logout"
